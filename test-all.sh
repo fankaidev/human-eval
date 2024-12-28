e617d062a666e5f90ac542a21fa44420cfc855af
fankai <fankaidev@gmail.com>
2024-12-28 10:03:28 +0800

    update deepseek-v3

--- test-all.sh
+++ test-all.sh
@@ -9,7 +9,7 @@ for model in llama3.2:3b llama3.1:8b qwen2.5:3b qwen2.5:7b qwen2.5:14b qwen2.5-c
     python code_and_eval.py --model $model --use_ollama
 done
 
-for model in llama3.1-8b llama3.3-70b claude-3.5-haiku gpt-4o-mini gemini-2.0-flash gemini-1.5-pro gemma2-9b qwen-coder qwen-72b yi-large-turbo deepseek-coder; do
+for model in llama3.1-8b llama3.3-70b claude-3.5-haiku gpt-4o-mini gemini-2.0-flash gemini-1.5-pro gemma2-9b qwen-coder qwen-72b yi-large-turbo deepseek-coder deepseek-chat; do
     if [ -f "data/HumanEval_samples_${model}_summary.json" ]; then
         echo "skip $model"
         continue
